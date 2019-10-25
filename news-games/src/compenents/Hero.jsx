132e9ea416501121567c834ae49234a39f9e61da
Winston Meikle <winmeikle@gmail.com>
2019-10-25 01:10:06 -0400

    more sounds

--- news-games/src/compenents/Hero.jsx
+++ news-games/src/compenents/Hero.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Nav from './Nav'
 import mario from '../mario.mp3';
+import ps2 from '../ps2.mp3';
+import psp from '../psp.mp3';
+import gb from '../gb.mp3';
+import sega from '../sega.mp3';
+import gba from '../gba.mp3';
+import xbox from '../xbox.mp3';
 
 class Hero extends React.Component {
   constructor(props) {
@@ -9,7 +15,12 @@ class Hero extends React.Component {
   }
     render() {
       let audioMario = new Audio(mario);
-  
+      let audioPS2 = new Audio(ps2);
+      let audioPSP = new Audio(psp);
+      let audioSega = new Audio(sega);
+      let audioGb = new Audio(gb);
+      let audioGba = new Audio(gba);
+      let audioXbox = new Audio(xbox);
       return (
         <section id='hero'>
           <div id='hero-image'>
@@ -25,6 +36,31 @@ class Hero extends React.Component {
 
       
             </form>
+            
+          <div id="sounds">
+            <div className="sound">
+              <input  type="radio" name="games" onClick={async () => await audioPS2.play()} />
+            </div>
+
+            <div className="sound">
+              <input  type="radio" name="games" onClick={async () => await audioPSP.play()}/>
+            </div>
+            <div className="sound">
+              <input  type="radio"
+                name="games" onClick={async () => await audioGba.play()}/>
+            </div>
+            <div className="sound">
+              <input  type="radio" name="games" onClick={async () => await audioSega.play()}/>
+            </div>
+            <div className="sound">
+              <input  type="radio" name="games"
+                onClick={async () => await audioGb.play()}/>
+            </div>
+            <div className="sound">
+              <input  type="radio" name="games" onClick={async () => await audioXbox.play()}/>
+            </div>
+
+            </div>
           </div>
 
         </section>
