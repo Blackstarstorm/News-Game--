1aedcd1e93db346d384842c02ebb66e8461ca08a
Winston Meikle <winmeikle@gmail.com>
2019-12-02 00:08:35 -0500

    works

--- news-games/src/services/api-helper.js
+++ news-games/src/services/api-helper.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const getArticles = async function (value,search) {
   const response = await axios(`https://newsapi.org/v2/everything?sources=${value}&q=${search}&apiKey=f475e555aa7945feb889e93489b953e8`);
-  console.log(response.data)
   return response.data.articles;
 }
 
